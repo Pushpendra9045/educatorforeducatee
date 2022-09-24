2fca2df0b0e4817f0f979ac92730bcb9e66cb055
HariKiran <harikiran.kante@gmail.com>
2022-09-25 00:23:33 +0530

    added footer

--- src/Components/student_mainpage/index.js
+++ src/Components/student_mainpage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../header";
-
+import Footer from "../footer/index";
 import "./student_mainpage.css";
 
 const Studentmainpage = () => {
@@ -10,6 +10,7 @@ const Studentmainpage = () => {
       <div className="askquest">
         <h1>hello</h1>
       </div>
+      <Footer />
     </>
   );
 };
