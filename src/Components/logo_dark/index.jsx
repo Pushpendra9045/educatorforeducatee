1ae0cfd9e02c36966807f9543649c9d1364b7408
HariKiran <harikiran.kante@gmail.com>
2022-09-24 23:58:34 +0530

    added logo and header

---
+++ src/Components/logo_dark/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./logo.css";
+const LogoDark = () => {
+  return (
+    <div className="logo_dark">
+      <div className="logo_sign">
+        <p>E</p>
+        <p>E</p>
+      </div>
+      <div className="logo_text">
+        <p>
+          {" "}
+          <span> Educator </span>FOR
+        </p>
+        <p>Educatee</p>
+      </div>
+    </div>
+  );
+};
+
+export default LogoDark;
